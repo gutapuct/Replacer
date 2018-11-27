dc2e986544b7123c0b86212ce81af41a6f37e565
Zakhar Aleshkin <gutapuct@mail.ru>
2018-11-27 19:15:08 +0300

    Added model for reason and moved the logic from controller to model

--- Replacer/Replacer/Controllers/ReasonsController.cs
+++ Replacer/Replacer/Controllers/ReasonsController.cs
@@ -7,51 +7,35 @@ namespace Replacer
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class ReasonsController : ApiController
     {
-        private static List<string> _reasons = new List<string> { "val1", "val2" };
+        private static Reason _model = new Reason();
         // GET api/reasons
         public IEnumerable<string> Get()
         {
-            return _reasons;
+            return _model.GetAllReasons();
         }
 
         // GET api/reasons/5
         public string Get(int id)
         {
-            if (_reasons.Count > id && id >= 0)
-            {
-                return _reasons[id];
-            }
-            else
-            {
-                return "Error. We couldn't find that reason.";
-            }
+            return _model.GetReason(id);
         }
 
         // POST api/reasons
         public void Post([FromBody]string reason)
         {
-            if (!string.IsNullOrWhiteSpace(reason))
-            {
-                _reasons.Add(reason);
-            }
+            _model.AddReason(reason);
         }
 
         // PUT api/reasons/5
         public void Put(int id, [FromBody]string reason)
         {
-            if (_reasons.Count > id && id >= 0)
-            {
-                _reasons[id] = reason;
-            }
+            _model.ChangeReason(id, reason);
         }
 
         // DELETE api/values/5
         public void Delete (int id)
         {
-            if (_reasons.Count > id && id >= 0)
-            {
-                _reasons.RemoveAt(id);
-            }
+            _model.DeleteReason(id);
         }
     }
 }
